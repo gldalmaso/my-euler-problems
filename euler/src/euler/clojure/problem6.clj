9a73ef3efef105ffd2249dd7f0bb5ea944d505e3
gustavo <gldalmaso@gmail.com>
2012-02-01 20:26:09 -0200

    problem 6

---
+++ euler/src/euler/clojure/problem6.clj
@@ -0,0 +1,22 @@
+(ns euler.clojure.problem6
+   "The sum of the squares of the first ten natural numbers is,
+   1^2 + 2^2 + ... + 10^2 = 385
+   The square of the sum of the first ten natural numbers is,
+   (1 + 2 + ... + 10)^2 = 55^2 = 3025
+   Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+   Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.")
+
+(defn sqre [x] (* x x))
+
+(defn sumsqre [range]
+  (apply + (map sqre range)))
+
+(defn sqresum [range]
+  (sqre (apply + range)))
+
+
+ (defn dif-sumsqre-sqresum [range]
+   (- (sqresum range)
+      (sumsqre range)))
+
+(print (dif-sumsqre-sqresum (range 1 101)))
